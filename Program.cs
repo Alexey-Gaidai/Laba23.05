225b988736fd83a6be30dcf8486e79586c5621e9
Alexey-Gaidai <71748686+Alexey-Gaidai@users.noreply.github.com>
2021-05-23 17:43:58 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laba23._05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
